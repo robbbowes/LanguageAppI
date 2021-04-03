7b11f004a70715d5aa752d5ea2113d05cc33e3ab
robbbowes <up666724@myport.ac.uk>
2021-04-03 14:46:30 +0200

    Metadata stripping works, audio works

--- App.tsx
+++ App.tsx
@@ -6,29 +6,27 @@ import { Sentence } from './models/Sentence'
 import FlashCard from './components/FlashCard'
 
 export default function App() {
-    const sentenceData: Sentence[] = [
-        {
-            id: 1,
-            text: 'Jeg har en hytte med bare tre rom.',
-            translations: [
-                {
-                    id: 1,
-                    language: Language.English,
-                    text: 'I have a cabin with only three rooms',
-                },
-                {
-                    id: 2,
-                    language: Language.French,
-                    text: "J'ai un chalet avec seulement trois chambres.",
-                },
-            ],
-            audio: './assets/assimil/lesson4/T03.mp3',
-        },
-    ]
+    const sentenceData: Sentence = {
+        id: 1,
+        text: 'Jeg har en hytte med bare tre rom.',
+        translations: [
+            {
+                id: 1,
+                language: Language.English,
+                text: 'I have a cabin with only three rooms',
+            },
+            {
+                id: 2,
+                language: Language.French,
+                text: "J'ai un chalet avec seulement trois chambres.",
+            },
+        ],
+        audio: './assets/assimil/lesson4/T03.mp3',
+    }
 
     return (
         <View style={styles.container}>
-            <FlashCard sentence={sentenceData[0]} />
+            <FlashCard sentence={sentenceData} />
         </View>
     )
 }
--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -3,19 +3,20 @@ import { StyleSheet, TouchableOpacity, View } from 'react-native'
 import { MaterialCommunityIcons, FontAwesome5 } from '@expo/vector-icons'
 import { Audio } from 'expo-av'
 import { AVPlaybackSource } from 'expo-av/build/AV'
-
+import MusicInfo from 'expo-music-info'
+import { Asset, useAssets } from 'expo-asset';
 interface AudioPlayerProps {
     src: AVPlaybackSource
 }
 
 const AudioPlayer = (props: AudioPlayerProps) => {
     const [sound, setSound] = useState(new Audio.Sound())
-    const { src } = props;
+    const { src } = props
+    // const [metaGrabber, setMetaGrabber] = useState(RNMusicMetadata)
+
 
     async function playSound() {
-        const { sound } = await Audio.Sound.createAsync(
-            src
-        )
+        const { sound } = await Audio.Sound.createAsync(src)
         setSound(sound)
         await sound.replayAsync()
     }
@@ -28,6 +29,19 @@ const AudioPlayer = (props: AudioPlayerProps) => {
             : undefined
     }, [sound])
 
+    // const path = './assets/assimil/lesson4/T03.mp3'
+    const meta = async () => {
+        const [{ localUri }] = await Asset.loadAsync(src);
+        console.log(localUri)
+        const data = await MusicInfo.getMusicInfoAsync(
+            localUri, 
+            {
+            title: true
+        })
+        console.log(data)
+        return data
+    }
+
     return (
         <View>
             <View style={styles.controls}>
@@ -35,6 +49,13 @@ const AudioPlayer = (props: AudioPlayerProps) => {
                     <FontAwesome5 name="play" size={20} color="#444" />
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.control} onPress={() => {}}>
+                    <MaterialCommunityIcons
+                        name="record-rec"
+                        size={28}
+                        color="#444"
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.control} onPress={meta}>
                     <MaterialCommunityIcons
                         name="record-rec"
                         size={28}
